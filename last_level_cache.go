db896ec396c0341bc9db46378483f29850b17f05
kanataxa <sota.corei324@gmail.com>
2019-11-11 16:11:44 +0900

    use lockexpiration

--- last_level_cache.go
+++ last_level_cache.go
@@ -84,7 +84,7 @@ func (c *LastLevelCache) Create(tx *Tx, tag, key string, value Type, expiration 
 	tx.stash.lastLevelCacheKeyToBytes[keyStr] = content
 	if c.opt.pessimisticLock {
 		if _, exists := tx.pendingQueries[keyStr]; !exists {
-			if err := c.lockKey(tx, cacheKey, expiration); err != nil {
+			if err := c.lockKey(tx, cacheKey, c.opt.lockExpiration); err != nil {
 				return xerrors.Errorf("failed to lock key: %w", err)
 			}
 		}
@@ -143,7 +143,7 @@ func (c *LastLevelCache) Update(tx *Tx, tag, key string, value Type, expiration 
 	keyStr := cacheKey.String()
 	if c.opt.pessimisticLock {
 		if _, exists := tx.pendingQueries[keyStr]; !exists {
-			if err := c.lockKey(tx, cacheKey, expiration); err != nil {
+			if err := c.lockKey(tx, cacheKey, c.opt.lockExpiration); err != nil {
 				return xerrors.Errorf("failed to lock key: %w", err)
 			}
 		}
